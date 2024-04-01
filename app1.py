32ce7850012907b98e98d41357f935b900a91a31
s20278 <157474371+s20278@users.noreply.github.com>
2024-04-01 12:43:40 +0200

    Add files via upload

--- app1.py
+++ app1.py
@@ -35,7 +35,7 @@ def main():
         sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
 
     with right:
-        age_slider = st.slider("Wiek", value=20, min_value=1, max_value=70.5)
+        age_slider = st.slider("Wiek", min_value=1, max_value=70.5)
         sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
         parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
         fare_slider = st.slider("Cena biletu", min_value=0, max_value=93.5, step=1)
