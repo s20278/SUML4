22debe8364b5fa3a6e29c3e5acb5b4d5bd2eda6a
s20278 <157474371+s20278@users.noreply.github.com>
2024-04-01 13:22:33 +0200

    Add files via upload

--- app1.py
+++ app1.py
@@ -12,15 +12,13 @@ pclass_d = {1: "Pierwsza", 2: "Druga", 3: "Trzecia"}
 embarked_d = {"C": "Cherbourg", "Q": "Queenstown", "S": "Southampton"}
 sex_d = {"female": "Kobieta", "male": "Mężczyzna"}
 
-
 def main():
     st.set_page_config(page_title="Titanic Survival Predictor")
 
     st.title("Titanic Survival Predictor")
 
-# Dodanie zdjęcia
-st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_3.jpg/250px-RMS_Titanic_3.jpg", caption="Statek Titanic")
-
+    # Dodanie zdjęcia
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_3.jpg/250px-RMS_Titanic_3.jpg", caption="Statek Titanic")
 
     # Wybór cech przez użytkownika
     pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
@@ -59,7 +57,6 @@ st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_
     st.subheader("Czy taka osoba przeżyłaby katastrofę?")
     st.subheader("Tak" if survival[0] == 1 else "Nie")
     st.write("Pewność predykcji: {0:.2f} %".format(s_confidence[0][survival][0] * 100))
-
 
 if __name__ == "__main__":
     main()
