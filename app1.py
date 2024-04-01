aa7381d8e9be1bbc307cf32f95d4b41bbceeb71e
s20278 <157474371+s20278@users.noreply.github.com>
2024-04-01 12:51:23 +0200

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,21 +1,22 @@
 import streamlit as st
-import pickle
+import joblib
 from datetime import datetime
 
-# import bibliotek
+# import znanych nam bibliotek
 import pathlib
 from pathlib import Path
 
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
-# Wczytaj model
-filename = "model.pkl"  # Poprawiona nazwa pliku
-model = pickle.load(open(filename, 'rb'))
+filename = "model.sv"
+# Wczytanie modelu za pomocą joblib.load
+model = joblib.load(filename)
 
-# Słowniki dla konwersji danych
-pclass_d = {1: "Pierwsza", 2: "Druga", 3: "Trzecia"}
-embarked_d = {"C": "Cherbourg", "Q": "Queenstown", "S": "Southampton"}
+# Pozostała część skryptu pozostaje bez zmian
+
+pclass_d = {1: "Pierwsza", 2: "Druga", 3: "Trzecia"}  
+embarked_d = {"C":"Cherbourg", "Q":"Queenstown", "S":"Southampton"}
 sex_d = {"female": "Kobieta", "male": "Mężczyzna"}
 
 def main():
@@ -30,14 +31,14 @@ def main():
         st.title("Titanic Survival Predictor")
 
     with left:
-        pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
+        pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])  
         sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
 
     with right:
-        age_slider = st.slider("Wiek", min_value=1, max_value=100)  # Zwiększony zakres wieku
-        sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=10)
+        age_slider = st.slider("Wiek", min_value=1, max_value=70.5)
+        sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
         parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
-        fare_slider = st.slider("Cena biletu", min_value=0, max_value=513, step=1)  # Zwiększony zakres ceny biletu
+        fare_slider = st.slider("Cena biletu", min_value=0, max_value=93.5, step=1)
 
     embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2, format_func=lambda x: embarked_d[x])
 
@@ -48,7 +49,7 @@ def main():
     with prediction:
         st.subheader("Czy taka osoba przeżyłaby katastrofę?")
         st.subheader(("Tak" if survival[0] == 1 else "Nie"))
-        st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival[0]] * 100))  # Poprawka indeksu
+        st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
 if __name__ == "__main__":
     main()
